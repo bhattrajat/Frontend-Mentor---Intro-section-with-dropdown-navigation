08e0376b7ed9590ac7b89a65ec363cafc0a091d4
Rajat Bhatt <rajatbhatt999@gmail.com>
2022-04-25 20:41:27 +0530

    Add basePath and assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  basePath: isProd
+    ? '/Frontend-Mentor---Intro-section-with-dropdown-navigation'
+    : '/',
+  assetPrefix: isProd
+    ? '/Frontend-Mentor---Intro-section-with-dropdown-navigation'
+    : '',
 }
